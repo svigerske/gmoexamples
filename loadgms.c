a74545358122115d8a58be9eb5cff47095d666e3
Stefan Vigerske <svigerske@gams.com>
2022-03-11 15:17:25 +0100

    create proper temp dir in loadgms; adjust to newer convert
    - avoid creating gams.gms and dict.txt in working dir

--- loadgms.c
+++ loadgms.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <dirent.h>
 
 #include "gmomcc.h"
 #include "gevmcc.h"
@@ -8,37 +8,39 @@
 
 #include "loadgms.h"
 
-// set to 3 to see gams log
+/* set to 3 to see gams log */
 #define GAMSLOGOPTION 0
 
 RETURN loadGMS(
    struct gmoRec** gmo,
    struct gevRec** gev,
-   const char* gmsfile
+   const char*     gmsfile
 )
 {
+   char scrdir[] = "/tmp/loadgmsXXXXXX";
+   char filename[35];
    char gamscall[1024];
    char buffer[GMS_SSSIZE];
+   FILE* convertopt;
    int rc;
    
-   FILE* convertdopt;
-   
    /* create temporary directory */
-   mkdir("loadgms.tmp", S_IRWXU);
+   mkdtemp(scrdir);
    
-   /* create empty convertd options file */
-   convertdopt = fopen("loadgms.tmp/convertd.opt", "w");
-   if( convertdopt == NULL )
+   /* create convert options file */
+   sprintf(filename, "%s/convert.opt", scrdir);
+   convertopt = fopen(filename, "w");
+   if( convertopt == NULL )
    {
-      fprintf(stderr, "Could not create convertd options file.\n");
+      fprintf(stderr, "Could not create convert options file %s.\n", filename);
       return RETURN_ERROR;
    }
-   fputs(" ", convertdopt);
-   fclose(convertdopt);
+   /* with new convert, something needs to be created to avoid writing gams.gms and dict.txt into working dir */
+   fprintf(convertopt, "dict %s/dict\n", scrdir);
+   fclose(convertopt);
    
-   /* call GAMS with convertd solver to get compiled model instance in temporary directory */
-   snprintf(gamscall, sizeof(gamscall), GAMSDIR "/gams %s LP=CONVERTD RMIP=CONVERTD QCP=CONVERTD RMIQCP=CONVERTD NLP=CONVERTD DNLP=CONVERTD RMINLP=CONVERTD CNS=CONVERTD MIP=CONVERTD MIQCP=CONVERTD MINLP=CONVERTD MCP=CONVERTD MPEC=CONVERTD RMPEC=CONVERTD SCRDIR=loadgms.tmp output=loadgms.tmp/listing optdir=loadgms.tmp optfile=1 pf4=0 solprint=0 limcol=0 limrow=0 pc=2 lo=%d", gmsfile, GAMSLOGOPTION);
-   /* printf(gamscall); fflush(stdout); */
+   /* call GAMS with convert solver to get compiled model instance in temporary directory */
+   snprintf(gamscall, sizeof(gamscall), "\"" GAMSDIR "/gams\" %s SOLVER=CONVERT SCRDIR=%s output=%s/listing optdir=%s optfile=1 pf4=0 solprint=0 limcol=0 limrow=0 pc=2 lo=%d", gmsfile, scrdir, scrdir, scrdir, GAMSLOGOPTION);
    rc = system(gamscall);
    if( rc != 0 )
    {
@@ -49,15 +51,16 @@ RETURN loadGMS(
    /* initialize GMO and GEV libraries */
    if( !gmoCreateDD(gmo, GAMSDIR, buffer, sizeof(buffer)) || !gevCreateDD(gev, GAMSDIR, buffer, sizeof(buffer)) )
    {
-      fprintf(stderr, buffer);
-      fprintf(stderr, "\n");
+      fputs(buffer, stderr);
+      fputs("\n", stderr);
       return 1;
    }
 
    /* load control file */
-   if( gevInitEnvironmentLegacy(*gev, "loadgms.tmp/gamscntr.dat") )
+   sprintf(filename, "%s/gamscntr.dat", scrdir);
+   if( gevInitEnvironmentLegacy(*gev, filename) )
    {
-      fprintf(stderr, "Could not load control file loadgms.tmp/gamscntr.dat\n");
+      fprintf(stderr, "Could not load control file %s\n", filename);
       gmoFree(gmo);
       gevFree(gev);
       return 1;
@@ -93,15 +96,40 @@ void freeGMS(
    struct gevRec** gev
 )
 {
+   char scrdirname[20];
+   char filename[275];
+   DIR* scrdir;
+   struct dirent* scrdirentry;
+
+   gevGetStrOpt(*gev, gevNameScrDir, scrdirname);
+
    gmoFree(gmo);
-   gmo = NULL;
+   *gmo = NULL;
    
    gevFree(gev);
-   gev = NULL;
+   *gev = NULL;
    
    gmoLibraryUnload();
    gevLibraryUnload();
    
-   /* remove temporary directory content (should have only files) and directory itself) */
-   system("rm loadgms.tmp/* && rmdir loadgms.tmp");
+   /* remove scratch dir content: should be only files */
+   scrdir = opendir(scrdirname);
+   if( scrdir != NULL )
+   {
+      while( (scrdirentry = readdir(scrdir)) != NULL )
+      {
+         if( scrdirentry->d_type != DT_DIR )  /* skip . and .. */
+         {
+            sprintf(filename, "%s%s", scrdirname, scrdirentry->d_name);
+            /* printf("Removing %s\n", filename); */
+            if( remove(filename) != 0 )
+               fprintf(stderr, "Could not remove temporary file %s\n", filename);
+         }
+      }
+   }
+   closedir(scrdir);
+
+   /* remove scratch dir: should be empty now */
+   if( remove(scrdirname) != 0 )
+      fprintf(stderr, "Could not remove temporary directory %s\n", scrdirname);
 }
